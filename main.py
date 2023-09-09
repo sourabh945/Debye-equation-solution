4576bfe6ea3b58281d365649fe4583a3701bbb92
Sourabh <sourabhsheokand945@gmail.com>
2023-09-09 21:24:23 +0530

    modified:   main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import datetime
+import csv
+import matplotlib.pyplot as graph
+
 e1 = []
 e2 = []
 w = []
@@ -34,5 +38,29 @@ einf = (sume2e2*sume1w - sume2e1w*sume2)/(sume2e2*sumw - sume2w*sume2)
 
 print("t = ",t)
 print("einf = " ,einf)
+
+rel_t = []
+
+for i in range(0,n):
+    t.append(e2[i]/((e1[i]-einf)*w[i]))
+
+with open(f'data({datetime.datetime.now()}).csv','w') as file:
+    writer = csv.writer(file,delimiter='/t')
+    writer.writerows(zip(w,rel_t))
+    file.close()
+
+graph.plot(w,rel_t)
+graph.xlabel("frequenecy")
+graph.ylabel("relaxation time")
+graph.title("relaxation time period variation with freq")
+graph.show()
+
+t = rel_t[0]
+
+for i in range(1,n):
+    t = t + rel_t[i]
+
+print('t avg = ' , t/float(n))
+
 
 
